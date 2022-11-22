3de346097992cb651b90a957ebaa11fc9ac69f8f
Mark <mjagerton@ucdavis.edu>
2022-11-22 10:53:39 -0800

    tests run locally

--- src/FARMTest.jl
+++ src/FARMTest.jl
@@ -1,5 +1,33 @@
 module FARMTest
 
-# Write your package code here.
+using Distributed, SlurmClusterManager, Dates
+
+"workers() but excluding master"
+getworkers() = filter(i -> i != 1, workers())
+
+function start_up_workers(ENV::Base.EnvDict; nprocs = Sys.CPU_THREADS)
+    # send primitives to workers
+    oldworkers = getworkers()
+    println_time_flush("removing workers $oldworkers")
+    rmprocs(oldworkers)
+    flush(stdout)
+    
+    if "SLURM_JOBID" in keys(ENV)
+        num_cpus_to_request = parse(Int, ENV["SLURM_TASKS_PER_NODE"])
+        println_time_flush("requesting $(num_cpus_to_request) cpus from slurm.")
+        pids = addprocs(SlurmManager(; verbose=true))
+    else
+        cputhrds = Sys.CPU_THREADS
+        cputhrds < nprocs && @warn "using nprocs = $cputhrds < $nprocs specified"
+        pids = addprocs(min(nprocs, cputhrds))
+    end
+    println_time_flush("Workers added: $pids")
+    return pids
+end
+
+function println_time_flush(str)
+    println(Dates.format(now(), "HH:MM:SS   ") * str)
+    flush(stdout)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,11 @@ using Distributed, SlurmClusterManager
 
 @testset "FARMTest.jl" begin
     
-    pids1 = addprocs(SlurmManager(; verbose=true))
-    @everywhere println("hello from $(myid()):$(gethostname())")
-    rmprocs(pids1)
+    if "SLURM_JOBID" in keys(ENV)
+        pids1 = addprocs(SlurmManager(; verbose=true))
+        @everywhere println("hello from $(myid()):$(gethostname())")
+        rmprocs(pids1)
+    end
 
     pids2 = FARMTest.start_up_workers(ENV)
     @everywhere println("hello again from $(myid()):$(gethostname())")
