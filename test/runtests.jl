222259d4ae618ead903b4b9cc1e7bc364a36cb67
Mark <mjagerton@ucdavis.edu>
2022-11-22 12:57:09 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,28 +3,24 @@ using Test
 using Distributed, SlurmClusterManager
 
 @testset "FARMTest.jl" begin
-    
-    if "SLURM_JOBID" in keys(ENV)
-        np = parse(Int, ENV["SLURM_NTASKS"])
-    else
-        np = Sys.CPU_THREADS
-    end
-    println_time_flush("regular addprocs")
-    pids0 = addprocs(np)
-    @everywhere println("hello from $(myid()):$(gethostname())")
-    rmprocs(pids0)
 
+    # SLURM only startup
     if "SLURM_JOBID" in keys(ENV)
         println_time_flush("slurm addprocs")
         pids1 = addprocs(SlurmManager(; verbose=true))
         @everywhere println("hello from $(myid()):$(gethostname())")
+        println_time_flush("removing procs $pids1")
         rmprocs(pids1)
     end
 
+    # using FARMTest
     println_time_flush("startup workers")
     pids2 = FARMTest.start_up_workers(ENV)
     @everywhere println("hello again from $(myid()):$(gethostname())")
+    println_time_flush("removing procs $pids2")
     rmprocs(pids2)
+
+
     println_time_flush("done")
 
 end
